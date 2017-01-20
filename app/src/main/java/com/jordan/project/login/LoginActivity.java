ad490505563dc49d6d7443cd7a816bcce8071733
Alx Slash <Alx Slash>
2017-01-20 21:28:20 +0800

    Initial commit

---
+++ app/src/main/java/com/jordan/project/login/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.jordan.project.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.jordan.project.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
