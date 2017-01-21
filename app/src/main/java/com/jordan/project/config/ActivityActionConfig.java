6f733f0a697c48f9c0565524fd496a5fb720531d
safari12580 <safari12580@hotmail.com>
2017-01-21 11:33:17 +0800

    Add activity config and some activity utils

---
+++ app/src/main/java/com/jordan/project/config/ActivityActionConfig.java
@@ -0,0 +1,14 @@
+package com.jordan.project.config;
+
+/**
+ * Created by icean on 2017/1/21.
+ */
+
+public final class ActivityActionConfig {
+    public static final String ACTION_TO_LOGIN = "com.jordan.project.LOGIN_ACTION";
+    public static final int REQUEST_CODE_LOGIN = 10000;
+    public static final int RESULT_CODE_LOGIN_SUCCESS = 10000;
+    public static final int RESULT_CODE_LOGIN_FALSE = -10000;
+    public static final String KEY_LOGIN_USER_NAME = "user_name";
+    public static final String KEY_LOGIN_USER_PASSWORD = "user_password";
+}
---
+++ app/src/main/java/com/jordan/project/utils/ActivityUtils.java
@@ -0,0 +1,25 @@
+package com.jordan.project.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.jordan.project.config.ActivityActionConfig;
+
+/**
+ * Created by icean on 2017/1/21.
+ */
+
+public final class ActivityUtils {
+    public static boolean startLoginActivitySafe(Activity ctx, String user_name, String user_password) {
+        Intent start_login_activity_intent = new Intent(ActivityActionConfig.ACTION_TO_LOGIN);
+        start_login_activity_intent.putExtra(ActivityActionConfig.KEY_LOGIN_USER_NAME, user_name);
+        start_login_activity_intent.putExtra(ActivityActionConfig.KEY_LOGIN_USER_PASSWORD, user_password);
+        try {
+            ctx.startActivityForResult(start_login_activity_intent, ActivityActionConfig.REQUEST_CODE_LOGIN);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
