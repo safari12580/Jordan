28da5262b9cf41c4815caf9697356261a75ccb27
safari12580 <safari12580@hotmail.com>
2017-01-23 19:12:06 +0800

    Modify struck of project

--- app/src/main/java/com/jordan/project/activities/LoginActivity.java
+++ app/src/main/java/com/jordan/project/activities/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.jordan.project.login;
+package com.jordan.project.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
---
+++ app/src/main/java/com/jordan/project/activities/RegistActivity.java
@@ -0,0 +1,15 @@
+package com.jordan.project.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jordan.project.R;
+
+public class RegistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regist);
+    }
+}
